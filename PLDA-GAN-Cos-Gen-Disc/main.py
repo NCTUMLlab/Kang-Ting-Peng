46d6c23a0a1d1933419d88026b1c306211cf2ccb
NCTU-MLLab <mllabjtc@gmail.com>
2017-10-05 16:27:30 +0800

    Update main.py

--- PLDA-GAN-Cos-Gen-Disc/main.py
+++ PLDA-GAN-Cos-Gen-Disc/main.py
@@ -21,7 +21,7 @@ def display_var(theta_var):
 
 if __name__ == '__main__':
 
-    dt = Dataset('/home/kt/NIST_npy/', one_hot=True)
+    dt = Dataset('../NIST_npy/', one_hot=True)
     mb_size = 100
     x_dim = dt.train_data.shape[1]
     y_dim = dt.train_label.shape[1]
